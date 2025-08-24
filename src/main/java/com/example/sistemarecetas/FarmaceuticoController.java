760555a282553ec875c76b487aa040a1a8c2e7d3
MARIANA MADRIGAL MOLINA <mariana.madrigal.molina@est.una.ac.cr>
2025-08-23 22:05:29 -0600

    Manejo de botones guardar y borrar en farmaceuticoController

--- src/main/java/com/example/sistemarecetas/FarmaceuticoController.java
+++ src/main/java/com/example/sistemarecetas/FarmaceuticoController.java
@@ -1,4 +1,32 @@
 package com.example.sistemarecetas;
 
-public class FarmaceuticoController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.RadioButton;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FarmaceuticoController implements Initializable {
+    @FXML
+    private RadioButton btnGuardarFarmaceuta;
+    @FXML
+    private RadioButton btnBorrarFarmaceuta;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // Listener para el botón de Guardar
+        btnGuardarFarmaceuta.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) { // Si el botón de guardar se acaba de seleccionar
+                btnBorrarFarmaceuta.setSelected(false); // Deselecciona el de Borrar
+            }
+        });
+
+        // Listener para el botón de Borrar
+        btnBorrarFarmaceuta.selectedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) { // Si el botón de borrar se acaba de seleccionar
+                btnGuardarFarmaceuta.setSelected(false); // Deselecciona el de Guardar
+            }
+        });
+    }
 }
